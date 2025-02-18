9fa8133c9158e3aa19ecc458c333d92be24b36eb
Remy Castella <remy.jpen@gmail.com>
2025-02-18 11:59:05 +0900

    set up seeds, configure models, generate controllers

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,42 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require "faker"
+require "open-uri" # to go at the top of the seed file
+
+Booking.destroy_all
+Clothing.destroy_all
+User.destroy_all
+
+puts "Creating 15 users..."
+15.times do
+  new_user = User.new(email: Faker::Internet.email, password: "123123")
+  new_user.save!
+end
+puts "#{User.all.count} users created"
+
+puts "Creating 20 clothing items..."
+clothing_items = [
+  "Kimono", "Hanbok", "Sari", "Qipao", "Dirndl", "Kilt", "Thobe", "Poncho", "Barong Tagalog", "Djellaba",
+  "Baju Melayu", "Ao Dai", "Dashiki", "Sarong", "Lehenga", "Gho", "Chapan", "Huipil", "Sombrero Vueltiao", "Shuka"
+]
+
+clothing_items.each do |item|
+  clothing = Clothing.new(name: item, price: rand(1000..10000), size: ["S", "M", "L"].sample)
+  clothing.user = User.all.sample
+  begin
+    file = URI.open(Faker::LoremFlickr.image(size: "100x100", search_terms: [item.downcase.gsub(" ", "_")]))
+    clothing.photo.attach(io: file, filename: "#{item.downcase.gsub(' ', '_')}.jpg", content_type: "image/jpg")
+  rescue OpenURI::HTTPError, SocketError => e
+    puts "Image fetch failed for #{item}: #{e.message}"
+  end
+  clothing.save!
+end
+
+puts "#{Clothing.all.count} clothes created"
+
+puts "Creating 10 bookings..."
+10.times do
+  booking = Booking.create!(user: User.all.sample, clothing: Clothing.all.sample)
+end
+puts "#{Booking.all.count} bookings created"
